f28e4e1a390587c7b1801b62149318e7beecfe34
Pauline Korukundo <pktpaulie@gmail.com>
2023-07-04 03:48:16 +0300

    ADD DEPLOYMENT DEPENDENCIES

--- calculator_proj/calculator_proj/settings.py
+++ calculator_proj/calculator_proj/settings.py
@@ -11,6 +11,13 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+
+import dj_database_url
+
+import environ
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -74,13 +81,16 @@ WSGI_APPLICATION = "calculator_proj.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
+""" DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
     }
-}
+} """
 
+DATABASES = {
+    'default': dj_database_url.parse(env('DATABASE_URL'))
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
